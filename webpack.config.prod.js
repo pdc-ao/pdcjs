4f9f4934fb9e87f00e2fd0b994fcb6ae48277daf
pdc-ao <pdc807751@gmail.com>
2025-10-23 22:26:47 +0100

    jsproject

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,25 +1,25 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
     new CopyPlugin({
       patterns: [
-        { from: 'img', to: 'img' },
-        { from: 'css', to: 'css' },
-        { from: 'js/vendor', to: 'js/vendor' },
-        { from: 'icon.svg', to: 'icon.svg' },
-        { from: 'favicon.ico', to: 'favicon.ico' },
-        { from: 'robots.txt', to: 'robots.txt' },
-        { from: 'icon.png', to: 'icon.png' },
+        { from: 'index.html', to: 'index.html' },
         { from: '404.html', to: '404.html' },
+        { from: 'favicon.ico', to: 'favicon.ico' },
+        { from: 'icon.png', to: 'icon.png' },
+        { from: 'icon.svg', to: 'icon.svg' },
+        { from: 'robots.txt', to: 'robots.txt' },
         { from: 'site.webmanifest', to: 'site.webmanifest' },
+        { from: 'css', to: 'css' },
+        { from: 'img', to: 'img' },
+        { from: 'js/vendor', to: 'js/vendor' },
+
+        // Optional: copy all your HTML views
+        { from: '*.html', to: '[name][ext]' },
       ],
     }),
   ],
